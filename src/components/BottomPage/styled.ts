206c81c30d833f6b57bb01d2ba516d864f7349e6
EuCaue <caueadriano.castro@gmail.com>
2022-10-28 18:27:58 -0300

    bottom page responsive

--- src/components/BottomPage/styled.ts
+++ src/components/BottomPage/styled.ts
@@ -3,13 +3,16 @@
 import styled from 'styled-components';
 import * as colors from '../../assets/styles/configColors/colors';
 import boostDesktop from '../../assets/images/bg-boost-desktop.svg';
+import boostMobile from '../../assets/images/bg-boost-mobile.svg';
 import { Link } from '../MiddlePage/styled';
 
 interface Icon {
   icon: string;
 }
 
+// whitehover on buttons
 export const WhiteHover = styled.span`
+  border-radius: 10em;
   border-radius: 2em;
   margin-top: 1.5em;
   :hover {
@@ -18,12 +21,13 @@ export const WhiteHover = styled.span`
   }
 `;
 
+// the blueline between the Background
 export const BlueLine = styled.div`
+  display: flex;
   position: absolute;
 
   z-index: -1;
-`;
-
+`; // Container Flex
 export const ContainerFlex = styled.section`
   display: flex;
   align-items: center;
@@ -32,6 +36,7 @@ export const ContainerFlex = styled.section`
   justify-content: center;
   position: relative;
 
+  /* The first boxWithIcon */
   div:nth-child(1) {
     margin-bottom: 4.8em;
     ${BlueLine} {
@@ -47,6 +52,7 @@ export const ContainerFlex = styled.section`
     }
   }
 
+  /* The second boxWithIcon */
   div:nth-child(2) {
     ${BlueLine} {
       width: 2.2em;
@@ -55,7 +61,9 @@ export const ContainerFlex = styled.section`
       margin-top: 5.5em;
       margin-left: 19.2em;
       z-index: -1;
-    }
+      }
+
+    /* IconSVG */
     span {
       top: 4.5em;
     }
@@ -64,14 +72,18 @@ export const ContainerFlex = styled.section`
     }
   }
 
+  /* The third boxWithIcon */
   div:nth-child(3) {
     margin-top: 9em;
+
+      /* IconSVG */
     span {
       top: 6.5em;
     }
   }
 `;
 
+// Background div for boxWithIcon
 export const Background = styled.div`
   background-color: #fff;
   display: flex;
@@ -85,6 +97,7 @@ export const Background = styled.div`
   border-radius: 4px;
 `;
 
+// IconSVG
 export const IconSVG = styled.span<Icon>`
   background-color: ${colors.darkViolet};
   background-image: url(${(props) => props.icon});
@@ -99,6 +112,7 @@ export const IconSVG = styled.span<Icon>`
   top: 2em;
 `;
 
+// H1 below the IconSVG
 export const H1 = styled.h1`
   padding-top: 3em;
   padding-left: 1em;
@@ -109,6 +123,7 @@ export const H1 = styled.h1`
   justify-content: center;
 `;
 
+// P below the IconSVG
 export const P = styled.p`
   color: ${colors.grayishViolet};
   font-weight: 500;
@@ -120,12 +135,14 @@ export const P = styled.p`
   text-align: left;
 `;
 
+// H1 for "Boost your links today"
 export const H1Boost = styled.h1`
   font-size: 2em;
   color: #fff;
   margin-right: 1em;
 `;
 
+// Backoground div image
 export const DivImage = styled.div`
   display: flex;
   justify-content: center;
@@ -142,6 +159,7 @@ export const DivImage = styled.div`
   background-color: ${colors.darkViolet};
 `;
 
+// "Get Started button"
 export const LinkBottom = styled(Link)`
   border-radius: 2em;
   margin-top: 0;
@@ -155,57 +173,76 @@ export const LinkBottom = styled(Link)`
   }
 `;
 
+// Container || MediaQueries
 export const Container = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-  /* width: 100%; */
-  /* height: 100%; */
   @media screen and (max-width: 60em) and (min-width: 20em) {
     ${ContainerFlex} {
       width: 90%;
       flex-direction: column;
     }
-    /* FIX: display !== none */
-    ${BlueLine} {
-      display: none;
-    }
+
     ${IconSVG} {
-      /* left: 0; */
       display: flex;
       align-self: center;
+      margin-left: 0;
     }
 
     ${Background} {
       justify-content: center;
       margin: 0;
-      /* width: 20em; */
-      /* margin-top: 4em; */
     }
     ${H1} {
       text-align: center;
       padding-top: 3.5em;
+      padding-left: 0;
     }
     ${P} {
+      /* text space padding */
       padding-left: 2em;
       padding-right: 2em;
       padding-bottom: 2.5em;
       text-align: center;
-    }
+  }
+
+  /* First boxWithIcon */
     div:nth-child(1) {
       margin-top: 4em;
+    ${BlueLine} {
+      width: 0;
+      margin: 0;
+      margin-top: 18em;
+      height: 3.5em;
+      align-self: center;
     }
+  }
+
+  /* Second boxWithIcon */
     div:nth-child(2) {
       display: flex;
       span {
         align-self: center;
         top: 21em;
       }
+    ${BlueLine} {
+      width: 0;
+      margin: 0;
+      margin-top: 19.3em;
+      height: 3.5em;
+      z-index: -1;
+      align-self: center;
     }
+}
+
+  /* Third boxWithIcon */
     div:nth-child(3) {
+      margin: 0;
+      margin-top: 4em;
       span {
-        top: 45.5em;
+        top: 41.5em;
         align-self: center;
       }
       ${P} {
@@ -213,5 +250,29 @@ export const Container = styled.div`
         padding-left: 1em;
       }
     }
-  }
+
+  ${DivImage} {
+    margin-top: 4em;
+    background-image: url(${boostMobile});
+    background-repeat: space;
+    background-size: cover;
+    background-position: left;
+    height: 16.5em;
+    }
+    ${LinkBottom} {
+      font-size: 1.25rem;
+    }
+
+    ${H1Boost} {
+      line-height: calc(100% + .5em);
+      font-size: 1.5rem;
+      text-align: center;
+      margin: 0;
+      width: 100%;
+    }
+
+    ${WhiteHover} {
+      margin-top: 1em;
+    }
+}
 `;
